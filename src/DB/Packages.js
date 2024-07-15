78ae92eaa93c514228b7fcab46c4d707ea8642f3
Yasser-Hemdaan <yasserhemdaan@gmail.com>
2024-07-15 05:54:15 +0300

    edit packages

--- src/DB/Packages.js
+++ src/DB/Packages.js
@@ -1,63 +1,63 @@
 const Packages = [
   {
     id: 1,
-    image: "/examinations/01.jpg",
+    image: "/packages/01.jpg",
   },
   {
     id: 2,
-    image: "/examinations/02.jpg",
+    image: "/packages/02.jpg",
   },
   {
     id: 3,
-    image: "/examinations/03.jpg",
+    image: "/packages/03.jpg",
   },
   {
     id: 4,
-    image: "/examinations/04.jpg",
+    image: "/packages/04.jpg",
   },
   {
     id: 5,
-    image: "/examinations/05.jpg",
+    image: "/packages/05.jpg",
   },
   {
     id: 6,
-    image: "/examinations/06.jpg",
+    image: "/packages/06.jpg",
   },
   {
     id: 7,
-    image: "/examinations/07.jpg",
+    image: "/packages/07.jpg",
   },
   {
     id: 8,
-    image: "/examinations/08.jpg",
+    image: "/packages/08.jpg",
   },
   {
     id: 9,
-    image: "/examinations/09.jpg",
+    image: "/packages/09.jpg",
   },
   {
     id: 10,
-    image: "/examinations/10.jpg",
+    image: "/packages/10.jpg",
   },
   {
     id: 11,
-    image: "/examinations/11.jpg",
+    image: "/packages/11.jpg",
   },
   {
     id: 12,
-    image: "/examinations/12.jpg",
+    image: "/packages/12.jpg",
   },
   {
     id: 13,
-    image: "/examinations/13.jpg",
+    image: "/packages/13.jpg",
   },
   {
     id: 14,
-    image: "/examinations/14.jpg",
+    image: "/packages/14.jpg",
   },
   {
     id: 15,
-    image: "/examinations/15.jpg",
+    image: "/packages/15.jpg",
   },
 ];
 
